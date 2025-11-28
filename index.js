e63cc72e532997440961059ce57bdfa1d4b17312
Zac Tranah <tranahzac@gmail.comgit config --global user.name zac-tranah>
2025-11-28 12:28:24 +0000

    my todos

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ const squares = document.getElementsByClassName("square"); //get all elements wi
 const players = ["X", "O"]; // 2 players: X and O in an array
 let currentPlayer = players[0]; //set the current player to X
 
-const endMessage = document.createElement("h2"); //create new html element 'h2'
-endMessage.textContent = `X's turn!`; //set the text content of the h2 element
-endMessage.style.marginTop = "30px"; // styling element
+const endMessage = document.createElement("h2"); 
+//create new html element 'h2'
+endMessage.textContent = `X's turn!`; 
+endMessage.style.marginTop = "30px"; // styling
 endMessage.style.textAlign = "center";
 board.after(endMessage); //insert the h2 element after the board element in the DOM
 
 for (let i = 0; i < squares.length; i++) {
   //loop through each square
-  squares[i].addEventListener("click", () => {
+  squares[i].addEventListener("click", () => { 
     //add click event listener to each square
     if (squares[i].textContent !== "") {
       //if the square is already filled
